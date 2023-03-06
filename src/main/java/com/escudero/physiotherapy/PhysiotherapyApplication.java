b5904f7fded270de9e67761418bb7cda23669a60
martin.escudero <martin.escudero@satec.es>
2023-03-06 13:08:10 +0100

    project

---
+++ src/main/java/com/escudero/physiotherapy/PhysiotherapyApplication.java
@@ -0,0 +1,13 @@
+package com.escudero.physiotherapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiotherapyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiotherapyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escudero/physiotherapy/PhysiotherapyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escudero.physiotherapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiotherapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
